a8f52373530414c3859b09f05bd394b6e00185e5
Caleb Sharp <epicsharp11@gmail.com>
2016-04-29 23:10:53 +0000

    Add /home as a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
+  get 'home', to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
