a11c6a894b8218e68845fa23a414f4d863452c4f
Caleb Sharp <epicsharp11@gmail.com>
2016-04-30 12:46:45 -0700

    Add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
 
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 
