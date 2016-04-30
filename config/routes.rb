4b5a15304cd0c479260b500dcb19a1c168a61ab1
Caleb Sharp <epicsharp11@gmail.com>
2016-04-30 12:43:24 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+
   get 'home', to: 'home#index'
+
+  get 'signup', to: 'users#new'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
