96f4390d46bb14e3dda631c5776f71862e8c33cb
Caleb Sharp <epicsharp11@gmail.com>
2016-04-30 12:52:23 -0700

    Fix stupid mistake :wink:

--- db/migrate/20160430193120_create_users.rb
+++ db/migrate/20160430193120_create_users.rb
@@ -2,10 +2,10 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :first_name
-      t.last_name :string
-      t.email :string
-      t.password_digest :string
-      t.role :string
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :role
 
       t.timestamps null: false
     end
