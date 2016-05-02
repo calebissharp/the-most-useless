cc5ee2c7e613bf8f4cb7fd21b3b6aead6a86d5fb
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 16:11:08 -0700

    Add semicolon

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var gulpSequence = require('gulp-sequence')
+var gulpSequence = require('gulp-sequence');
 
 gulp.task('build', function(cb) {
   var tasks = ['clean', ['images'], ['sass', 'browserify']];
