bc676b41463dce28234c9ff45b42a2a10f6ac01c
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 20:23:36 +0000

    Add watch task

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,9 @@
+var gulp   = require('gulp');
+var config = require('../config');
+var watch  = require('gulp-watch');
+
+gulp.task('watch', ['watchify','browserSync'], function(callback) {
+  watch(config.sass.src, function() { gulp.start('sass'); });
+  watch(config.images.src, function() { gulp.start('images'); });
+  watch(config.iconFont.src, function() { gulp.start('iconFont'); });
+});
