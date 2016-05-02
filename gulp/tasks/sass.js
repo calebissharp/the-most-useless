5ca52900cea45372d38affc2fd7dcaeaf548035a
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 16:10:28 -0700

    Handle sass errors

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,12 +4,14 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
+var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 
 gulp.task('sass', function() {
   return gulp.src(config.src)
     .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
+    .on('error', handleErrors)
     .pipe(autoprefixer({ browsers: ['last 2 version'] }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
