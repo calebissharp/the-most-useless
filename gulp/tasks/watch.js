b3749f13eab505091a66fee8d2aa8d3800f6e315
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 16:45:39 -0700

    Remove unneeded watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,5 +5,4 @@ var watch = require('gulp-watch');
 gulp.task('watch', ['watchify','browserSync'], function(callback) {
   watch(config.sass.src, function() { gulp.start('sass'); });
   watch(config.images.src, function() { gulp.start('images'); });
-  watch(config.iconFont.src, function() { gulp.start('iconFont'); });
 });
