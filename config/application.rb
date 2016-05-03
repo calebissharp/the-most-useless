91f56ce0792a698a76f4c2b94fd72a293e3f561f
Caleb Sharp <epicsharp11@gmail.com>
2016-05-02 17:29:03 -0700

    Change application configuration

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Workspace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
+    config.assets.paths << Rails.root.join("public", "assets", "javascripts")
   end
 end
